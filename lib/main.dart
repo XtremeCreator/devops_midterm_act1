e5cb3c947c1930fcb40aff61ba5b7bd64075340c
XtremeCreator <mikorivera178@gmail.com>
2024-02-23 00:12:02 +0800

    -Miko Rivera
    *pubspec add

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _HomepageState extends State<Homepage> {
               children: [
                 Icon(Icons.search,color: Colors.white,),
                 SizedBox(width: 5,),
-                Image.network('https://cdn-icons-png.flaticon.com/512/1383/1383267.png',height: 35,)
+                Image.asset('assets/reddit.png',height: 35)
               ],
             )
           ],
